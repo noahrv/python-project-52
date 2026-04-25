9f8a6b297f6d11ba93518f57fdba743a82bcbe27
noahrv <kawaii-93@mail.ru>
2026-04-25 09:34:38 +0300

    feat: добавлен серверный рендер с Bootstrap на главную страницу

--- core/views.py
+++ core/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 
 def home(request):
     return HttpResponse("Hello, Hexlet!")
+
+
+class HomeView(TemplateView):
+    template_name = "core/home.html"
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from core import views
+from core.views import HomeView
+
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('', HomeView.as_view(), name='home'),
 ]
