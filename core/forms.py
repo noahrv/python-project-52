4904902a852744cc5d600fe9bf3d54496f5d5bb7
noahrv <kawaii-93@mail.ru>
2026-04-26 18:52:03 +0300

    fix: исправлено отображение исполнителя в форме задачи

--- core/forms.py
+++ core/forms.py
@@ -70,6 +70,13 @@ class LabelForm(forms.ModelForm):
 
 
 class TaskForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['executor'].label_from_instance = (
+            lambda user: user.get_full_name() or user.username
+        )
+
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
