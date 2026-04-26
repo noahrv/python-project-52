46b56660cc31d2f67ca0c97be6c464fbb455a4ac
noahrv <kawaii-93@mail.ru>
2026-04-26 17:19:21 +0300

    feat: добавлена фильтрация задач и тесты

--- core/tests.py
+++ core/tests.py
@@ -131,3 +131,249 @@ class TaskCrudTest(TestCase):
         )
 
         self.assertContains(response, 'уже существует')
+
+    def test_filter_tasks_by_status(self):
+        another_status = Status.objects.create(name='В работе')
+        matching_task = Task.objects.create(
+            name='Подходит по статусу',
+            description='',
+            status=self.status,
+            author=self.author,
+        )
+        Task.objects.create(
+            name='Не подходит по статусу',
+            description='',
+            status=another_status,
+            author=self.author,
+        )
+        self.client.force_login(self.author)
+
+        response = self.client.get(
+            reverse('task_list'),
+            {'status': self.status.id},
+        )
+
+        self.assertContains(response, matching_task.name)
+        self.assertNotContains(response, 'Не подходит по статусу')
+
+    def test_filter_tasks_by_executor(self):
+        matching_task = Task.objects.create(
+            name='Подходит по исполнителю',
+            description='',
+            status=self.status,
+            author=self.author,
+            executor=self.other_user,
+        )
+        Task.objects.create(
+            name='Не подходит по исполнителю',
+            description='',
+            status=self.status,
+            author=self.author,
+            executor=self.author,
+        )
+        self.client.force_login(self.author)
+
+        response = self.client.get(
+            reverse('task_list'),
+            {'executor': self.other_user.id},
+        )
+
+        self.assertContains(response, matching_task.name)
+        self.assertNotContains(response, 'Не подходит по исполнителю')
+
+    def test_filter_tasks_by_label(self):
+        another_label = Label.objects.create(name='feature')
+        matching_task = Task.objects.create(
+            name='Подходит по метке',
+            description='',
+            status=self.status,
+            author=self.author,
+        )
+        matching_task.labels.add(self.label)
+
+        other_task = Task.objects.create(
+            name='Не подходит по метке',
+            description='',
+            status=self.status,
+            author=self.author,
+        )
+        other_task.labels.add(another_label)
+
+        self.client.force_login(self.author)
+
+        response = self.client.get(
+            reverse('task_list'),
+            {'label': self.label.id},
+        )
+
+        self.assertContains(response, matching_task.name)
+        self.assertNotContains(response, other_task.name)
+
+    def test_filter_only_self_tasks(self):
+        matching_task = Task.objects.create(
+            name='Моя задача',
+            description='',
+            status=self.status,
+            author=self.author,
+        )
+        Task.objects.create(
+            name='Чужая задача',
+            description='',
+            status=self.status,
+            author=self.other_user,
+        )
+        self.client.force_login(self.author)
+
+        response = self.client.get(
+            reverse('task_list'),
+            {'self_tasks': 'on'},
+        )
+
+        self.assertContains(response, matching_task.name)
+        self.assertNotContains(response, 'Чужая задача')
+
+    def test_filter_tasks_by_status_executor_and_label(self):
+        matching_task = Task.objects.create(
+            name='Подходит по всем фильтрам',
+            description='',
+            status=self.status,
+            author=self.author,
+            executor=self.other_user,
+        )
+        matching_task.labels.add(self.label)
+
+        Task.objects.create(
+            name='Не подходит по исполнителю',
+            description='',
+            status=self.status,
+            author=self.author,
+            executor=self.author,
+        )
+
+        self.client.force_login(self.author)
+
+        response = self.client.get(
+            reverse('task_list'),
+            {
+                'status': self.status.id,
+                'executor': self.other_user.id,
+                'label': self.label.id,
+            },
+        )
+
+        self.assertContains(response, matching_task.name)
+        self.assertNotContains(response, 'Не подходит по исполнителю')
+
+    def test_create_status(self):
+        self.client.force_login(self.author)
+
+        response = self.client.post(
+            reverse('status_create'),
+            {'name': 'Новый статус'},
+        )
+
+        self.assertRedirects(response, reverse('status_list'))
+        self.assertTrue(Status.objects.filter(name='Новый статус').exists())
+
+    def test_update_status(self):
+        status = Status.objects.create(name='Старый')
+        self.client.force_login(self.author)
+
+        response = self.client.post(
+            reverse('status_update', kwargs={'pk': status.pk}),
+            {'name': 'Готово'},
+        )
+
+        self.assertRedirects(response, reverse('status_list'))
+        status.refresh_from_db()
+        self.assertEqual(status.name, 'Готово')
+
+    def test_delete_status(self):
+        status = Status.objects.create(name='Удаляемый')
+        self.client.force_login(self.author)
+
+        response = self.client.post(
+            reverse('status_delete', kwargs={'pk': status.pk})
+        )
+
+        self.assertRedirects(response, reverse('status_list'))
+        self.assertFalse(Status.objects.filter(pk=status.pk).exists())
+
+    def test_cannot_delete_status_if_used(self):
+        status = Status.objects.create(name='Используется')
+        Task.objects.create(
+            name='Связана',
+            description='',
+            status=status,
+            author=self.author,
+        )
+        self.client.force_login(self.author)
+
+        response = self.client.post(
+            reverse('status_delete', kwargs={'pk': status.pk})
+        )
+
+        self.assertRedirects(response, reverse('status_list'))
+        self.assertTrue(Status.objects.filter(pk=status.pk).exists())
+
+    def test_create_label(self):
+        self.client.force_login(self.author)
+
+        response = self.client.post(
+            reverse('label_create'),
+            {'name': 'feature'},
+        )
+
+        self.assertRedirects(response, reverse('label_list'))
+        self.assertTrue(Label.objects.filter(name='feature').exists())
+
+    def test_update_label(self):
+        label = Label.objects.create(name='old_label')
+        self.client.force_login(self.author)
+
+        response = self.client.post(
+            reverse('label_update', kwargs={'pk': label.pk}),
+            {'name': 'feature'},
+        )
+
+        self.assertRedirects(response, reverse('label_list'))
+        label.refresh_from_db()
+        self.assertEqual(label.name, 'feature')
+
+    def test_delete_label(self):
+        label = Label.objects.create(name='Удаляемая')
+        self.client.force_login(self.author)
+
+        response = self.client.post(
+            reverse('label_delete', kwargs={'pk': label.pk})
+        )
+
+        self.assertRedirects(response, reverse('label_list'))
+        self.assertFalse(Label.objects.filter(pk=label.pk).exists())
+
+    def test_cannot_delete_label_if_used(self):
+        label = Label.objects.create(name='Используется')
+        task = Task.objects.create(
+            name='Связана',
+            description='',
+            status=self.status,
+            author=self.author,
+        )
+        task.labels.add(label)
+
+        self.client.force_login(self.author)
+
+        response = self.client.post(
+            reverse('label_delete', kwargs={'pk': label.pk})
+        )
+
+        self.assertRedirects(response, reverse('label_list'))
+        self.assertTrue(Label.objects.filter(pk=label.pk).exists())
+
+    def test_login_required_for_status_list(self):
+        response = self.client.get(reverse('status_list'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_login_required_for_label_list(self):
+        response = self.client.get(reverse('label_list'))
+        self.assertEqual(response.status_code, 302)
